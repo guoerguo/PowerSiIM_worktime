3ea6b0106ec7aaba4e6bd09a308c42dc3eef3e9a
guoerguo <62945625+guoerguo@users.noreply.github.com>
2021-07-29 09:40:59 +0800

    Add files via upload

---
+++ PowerSiIM_worktime.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         信息管理平台工时查询助手
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       cdsm652
+// @match        https://im.powersi.com/PowerSiIM/main.action
+// @icon         https://www.google.com/s2/favicons?domain=greasyfork.org
+// @grant        unsafeWindow
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    alert("同是天涯沦落人，相逢何必曾相识！")
+})();
